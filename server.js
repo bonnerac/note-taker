fa56321a1c383423fd63e75f5fdafc1d3189e3dd
bonnerac <alex.c.bonner@gmail.com>
2020-01-23 14:55:44 -0600

    cleaned some routes to push on heroku

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
+var fs = require("fs")
 // var mysql = require("mysql");
 
 // Set up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTML routes ====================================
-//   * GET `/notes` - Should return the `notes.html` file.
 app.use(express.static("public"));
-
+//   * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 //   * GET `*` - Should return the `index.html` file
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
+
 
 // API routes =====================================
 
 //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-app.get("/api/notes")
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        console.log(data)
+        res.json(data)
+    })
+});
 
 //   * POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+app.post("/api/notes", function (req, res) {
+    var newPost = req.body;
+    console.log(newPost);
+
+    // newPost.push(db.json)
+    res.send("data added")
+})
 
 //   * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// app.delete("/api/notes/:id", function (req, res) {
 
+// })
+
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 // Starts the server to begin listening
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
